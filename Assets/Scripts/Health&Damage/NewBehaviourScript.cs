7a9b69c90ce82a741f56910d119bdb23ea35d188
gzoukis <georgios.bazoukis@gmail.com>
2021-04-01 12:02:38 +0300

    initial commit

---
+++ Assets/Scripts/Health&Damage/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private GameObject enemyToKill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyToKill = GameObject.FindGameObjectWithTag("Boss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyToKill == null)
+        {
+            GameManager.instance.LevelCleared();
+        }
+    }
+}
